49f4977df372f9311aa21d1430728d3a13418f38
poornasamy <poornasamys@gmail.com>
2016-06-01 20:28:16 +0530

    Spring Basic Configuration

---
+++ src/com/tm/TaskController.java
@@ -0,0 +1,16 @@
+package com.tm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TaskController {
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public @ResponseBody String respondToPing()
+    {
+        return "Task manager responding to ping at " + System.currentTimeMillis();
+    }
+}
