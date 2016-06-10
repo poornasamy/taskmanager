c5ac6057470f96595282f259595301c0f923d429
poornasamy <poornasamys@gmail.com>
2016-06-10 18:58:24 +0530

    write to excel service

--- src/com/tm/TaskController.java
+++ src/com/tm/TaskController.java
@@ -1,10 +1,23 @@
 package com.tm;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.model.Response;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONArray;
 
 @Controller
 public class TaskController {
@@ -14,5 +27,27 @@ public class TaskController {
     {
         Logger.getLogger("tm").info("ping");
         return "Task manager responding to ping at " + System.currentTimeMillis();
+    }
+    
+    @RequestMapping(value = "tm/writetoexcel", method = RequestMethod.POST)
+    public @ResponseBody String respondToWriteToExcel(HttpServletRequest request, HttpServletResponse response) throws Throwable
+    {
+        Logger.getLogger("tm").info("writetoexcel");
+        
+        Response responseObj = new Response(HttpServletResponse.SC_OK, Response.RESPONSE_MSG_SUCCESS);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String exceldata = request.getParameter("exceldata");
+        
+        if(exceldata != null)
+        {
+            JSONArray excelJsonArray = new JSONArray();
+            
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            FileOutputStream out = new FileOutputStream(new File("employee.xlsx"));
+            workbook.write(out);
+            out.close();
+        }
+        String ResponseString = objectMapper.writeValueAsString(responseObj);
+        return ResponseString;
     }
 }
