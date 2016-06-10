43745419d45c04c03ebff6028f4169477d85c518
poornasamy <poornasamys@gmail.com>
2016-06-10 12:52:18 +0530

    Logger is implemented

--- src/com/tm/TaskController.java
+++ src/com/tm/TaskController.java
@@ -1,5 +1,6 @@
 package com.tm;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ public class TaskController {
     @RequestMapping(value = "tm/ping", method = RequestMethod.GET)
     public @ResponseBody String respondToPing()
     {
+        Logger.getLogger("tm").info("ping");
         return "Task manager responding to ping at " + System.currentTimeMillis();
     }
 }
